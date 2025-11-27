6dfbb4c7c3ab981df4fa9d56333c32d8f0eb28d5
JasterV <49537445+JasterV@users.noreply.github.com>
2025-11-27 11:04:54 +0100

    feat: implement EventBus

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-//! An implementation of a simple event bus.
+//! An implementation of a thread-safe event bus.
 //!
 //! It provides the following features:
 //!
 //! - Users can publish messages to a topic
-//! - Users can subscribe to events published to a certain topic.
+//! - Users can subscribe a topic and listen for incoming events.
 //!
 //! Messages are published as bytes, it is responsibility of the user to perform the encoding and decoding.
 use dashmap::DashMap;
@@ -28,16 +28,18 @@ struct Topic {
 
 /// A map keeping track of the existing topics.
 ///
-/// It contains the logic that understands when to create or delete a topic completely.
+/// It contains the logic that understands when to create or delete a topic.
 ///
 /// It makes use of a concurrent map that can be safely shared between threads.
-struct TopicMap {
-    inner: Arc<DashMap<String, Topic>>,
-}
+struct TopicMap(DashMap<String, Topic>);
 
 impl TopicMap {
-    pub(crate) fn new_subscriber(&self, topic_name: &str) -> Rx {
-        if let Some(topic) = self.inner.get(topic_name) {
+    fn get_sender(&self, topic_name: &str) -> Option<Tx> {
+        self.0.get(topic_name).map(|topic| topic.sender.clone())
+    }
+
+    fn new_subscriber(&self, topic_name: &str) -> Rx {
+        if let Some(topic) = self.0.get(topic_name) {
             topic
                 .subscribers
                 .fetch_add(1, std::sync::atomic::Ordering::Relaxed);
@@ -53,13 +55,13 @@ impl TopicMap {
             receiver: rx.resubscribe(),
         };
 
-        self.inner.insert(topic_name.to_string(), topic);
+        self.0.insert(topic_name.to_string(), topic);
 
         rx
     }
 
-    pub(crate) fn remove_subscriber(&self, topic_name: &str) {
-        let Some(topic) = self.inner.get(topic_name) else {
+    fn remove_subscriber(&self, topic_name: &str) {
+        let Some(topic) = self.0.get(topic_name) else {
             return ();
         };
 
@@ -67,34 +69,69 @@ impl TopicMap {
 
         if count <= 1 {
             let _ = topic.sender.downgrade();
-            let _ = self.inner.remove(topic_name);
+            let _ = self.0.remove(topic_name);
         }
     }
 }
 
 pub struct Subscription {
     topic: Arc<str>,
-    inner_topic_map: TopicMap,
+    inner_topics: Weak<TopicMap>,
     receiver: Rx,
 }
 
-// Implementation ideas:
-//
-// I.
-//
-// The event bus contains a map of (&str, Topic)
-//
-// A Topic could contain:
-//
-// - Arc<The `sender` used to send message>s
-// - The `receiver` that gets cloned
-// - A counter that keeps track of how many subscribers there are.
-//
-// Every time someone subscribes to a topic, it get's created in case it doesn't exist and the subscribers counter is sself.inner.get
-// If the topic already exists, the counter increases.
-// If the subscribers counter goes down to 0, the topic is removed.
-//
-// For the counter to go down, a subcription reference has to be dropped.
-//
-// We need tofind a way to decrease the counter automatically once the "subscription" gets dropped.
-// Then, somehow, when the counter get's down to 0, the topics map entry must be removed.
+impl Drop for Subscription {
+    fn drop(&mut self) {
+        if let Some(topics) = self.inner_topics.upgrade() {
+            topics.remove_subscriber(&self.topic);
+        }
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum PublishError {
+    #[error("Failed to publish message to topic: '{0}': '{1}")]
+    BroadcastError(Arc<str>, broadcast::error::SendError<Arc<[u8]>>),
+}
+
+/// A thread-safe event bus.
+///
+/// Users can subscribe to topics and publish to them.
+///
+/// When all the subscriptions to a topic get dropped, the topic itself is dropped from memory.
+pub struct EventBus {
+    topics: Arc<TopicMap>,
+}
+
+impl EventBus {
+    /// Subscribes to a topic and returns a `Subscription`.
+    ///
+    /// This operation will never fail, if the topic doesn't exist it gets internally created.
+    /// Once the subscription goes out of scope and there are no more references to the given topic,
+    /// the topic will automatically be dropped from memory.
+    pub fn subscribe(&self, topic: &str) -> Subscription {
+        let rx = self.topics.new_subscriber(topic);
+
+        Subscription {
+            topic: topic.into(),
+            inner_topics: Arc::downgrade(&self.topics),
+            receiver: rx,
+        }
+    }
+
+    /// Publishes a bunch of bytes to a topic.
+    ///
+    /// If the topic doesn't exist, nothing will happen and it won't be considered an error.
+    /// This method will only fail if, in case a topic with the given name exists, the internal `send` operation fails.
+    pub fn publish(&self, topic: &str, data: &[u8]) -> Result<(), PublishError> {
+        let Some(sx) = self.topics.get_sender(topic) else {
+            return Ok(());
+        };
+
+        let _ = sx
+            .send(Arc::from(data))
+            .map_err(|err| PublishError::BroadcastError(topic.into(), err))?;
+
+        Ok(())
+    }
+}

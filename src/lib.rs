e9ee13c8913cc5f9ea9bdc336f747baa853b068d
JasterV <49537445+JasterV@users.noreply.github.com>
2025-11-27 10:46:25 +0100

    feat: implement Topic, TopicMap & Subscriptioj

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,77 @@
 //! - Users can subscribe to events published to a certain topic.
 //!
 //! Messages are published as bytes, it is responsibility of the user to perform the encoding and decoding.
-use std::sync::mpsc;
+use dashmap::DashMap;
+use std::sync::{
+    Arc, Weak,
+    atomic::{AtomicUsize, Ordering},
+};
+use tokio::sync::broadcast;
 
-pub struct EventBus {}
+const TOPIC_CAPACITY: usize = 20;
+
+type Rx = broadcast::Receiver<Arc<[u8]>>;
+type Tx = broadcast::Sender<Arc<[u8]>>;
+
+/// Represents a single topic.
+/// Contains information about how many subscribers it has and the inner broadcast sender & receivers
+struct Topic {
+    subscribers: AtomicUsize,
+    sender: Tx,
+    receiver: Rx,
+}
+
+/// A map keeping track of the existing topics.
+///
+/// It contains the logic that understands when to create or delete a topic completely.
+///
+/// It makes use of a concurrent map that can be safely shared between threads.
+struct TopicMap {
+    inner: Arc<DashMap<String, Topic>>,
+}
+
+impl TopicMap {
+    pub(crate) fn new_subscriber(&self, topic_name: &str) -> Rx {
+        if let Some(topic) = self.inner.get(topic_name) {
+            topic
+                .subscribers
+                .fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+
+            return topic.receiver.resubscribe();
+        }
+
+        let (tx, rx) = broadcast::channel(TOPIC_CAPACITY);
+
+        let topic = Topic {
+            subscribers: AtomicUsize::new(1),
+            sender: tx,
+            receiver: rx.resubscribe(),
+        };
+
+        self.inner.insert(topic_name.to_string(), topic);
+
+        rx
+    }
+
+    pub(crate) fn remove_subscriber(&self, topic_name: &str) {
+        let Some(topic) = self.inner.get(topic_name) else {
+            return ();
+        };
+
+        let count = topic.subscribers.fetch_sub(1, Ordering::Relaxed);
+
+        if count <= 1 {
+            let _ = topic.sender.downgrade();
+            let _ = self.inner.remove(topic_name);
+        }
+    }
+}
+
+pub struct Subscription {
+    topic: Arc<str>,
+    inner_topic_map: TopicMap,
+    receiver: Rx,
+}
 
 // Implementation ideas:
 //
@@ -18,11 +86,11 @@ pub struct EventBus {}
 //
 // A Topic could contain:
 //
-// - The `sender` used to send messages
+// - Arc<The `sender` used to send message>s
 // - The `receiver` that gets cloned
 // - A counter that keeps track of how many subscribers there are.
 //
-// Every time someone subscribes to a topic, it get's created in case it doesn't exist and the subscribers counter is set to 1.
+// Every time someone subscribes to a topic, it get's created in case it doesn't exist and the subscribers counter is sself.inner.get
 // If the topic already exists, the counter increases.
 // If the subscribers counter goes down to 0, the topic is removed.
 //
@@ -30,20 +98,3 @@ pub struct EventBus {}
 //
 // We need tofind a way to decrease the counter automatically once the "subscription" gets dropped.
 // Then, somehow, when the counter get's down to 0, the topics map entry must be removed.
-
-impl EventBus {
-    pub fn subscribe(&self, _topic: impl AsRef<str>) -> Option<mpsc::Receiver<&[u8]>> {
-        todo!()
-    }
-
-    pub fn publish(&self, _topic: impl AsRef<str>) {}
-}
-
-#[cfg(test)]
-mod tests {
-
-    #[test]
-    fn it_works() {
-        assert!(true)
-    }
-}
